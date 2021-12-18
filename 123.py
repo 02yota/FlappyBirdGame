6b022e25398d8de718e082d29d146899d823be33
02yota <95132941+02yota@users.noreply.github.com>
2021-12-18 16:46:48 +0600

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,3 +1,16 @@
-import birdgame
-print(dir(birdgame))
-help(birdgame)
+from unittest import TestCase, main
+from birdgame import update_score
+
+class UpdateScoreTest(TestCase):
+    def test_more(self):
+        self.assertEqual(update_score(3,0),3)
+    def test_less(self):
+        self.assertEqual(update_score(0,3),3)
+    def test_equal(self):
+        self.assertEqual(update_score(3,3),3)
+
+
+
+
+if __name__ == 'main':
+    main()
